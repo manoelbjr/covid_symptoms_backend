27c318e971201f182c492d5f992b649114cad37e
Manoel Barros <manoel.barros.junior@gmail.com>
2020-11-06 00:00:39 -0300

    Adicionado  tabela assintomaticos

---
+++ insert.js
@@ -0,0 +1,18 @@
+const sqlite3 = require('sqlite3').verbose();
+
+// abrir banco de dados
+
+let db = new sqlite3.Database('./ubs.db');
+
+let sql = `INSERT INTO sintomaticos(nome,endereco,bairro,cidade) VALUES ('Mílton Belmonte', 'rua dos bobos', 'NUMERO ZERO', 'Springfield');`; 
+
+db.run(`INSERT INTO sintomaticos(nome,endereco,bairro,cidade) VALUES (?,?,?,?)`, 
+      ['Mílton Belmonte', 'rua dos bobos', 'NUMERO ZERO', 'Springfield'], 
+function(err){
+  if(err){
+      return console.log(err.message);
+  }
+  console.log(`Uma linha foi inserida`);
+})
+
+db.close();
