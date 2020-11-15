9cb138f799d5b194b2cb077f25aa18fb293bdb65
Manoel Barros <manoel.barros.junior@gmail.com>
2020-11-14 21:36:47 -0300

    Corrigida rota POST sintomaticos

---
+++ queries.sql
@@ -0,0 +1,16 @@
+CREATE TABLE sintomatico(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    endereco TEXT NOT NULL,
+    numero TEXT NOT NULL,
+    bairro TEXT NOT NULL,
+    cidade TEXT NOT NULL,
+    uf TEXT NOT NULL
+);
+
+INSERT INTO sintomatico (nome,endereco,numero,bairro,cidade,uf) VALUES ("Heloisa Fátima Teresinha","Rua Caetano Cavalcante","730","Cocó","Fortaleza","CE");
+INSERT INTO sintomatico (nome,endereco,numero,bairro,cidade,uf) VALUES ("Lívia Rafaela Yasmin da Silva","Rua Hélio Augusto de Rezende","609","Guarujá Mansões","Betim","MG");
+INSERT INTO sintomatico (nome,endereco,numero,bairro,cidade,uf) VALUES ("Caleb Nicolas Martins","Conjunto Paulo VI","553","Petrópolis","Manaus","AM");
+INSERT INTO sintomatico (nome,endereco,numero,bairro,cidade,uf) VALUES ("Bento Marcelo da Rosa","Rua Quatro","SN","Funcionários","Contagem","MG");
+INSERT INTO sintomatico (nome,endereco,numero,bairro,cidade,uf) VALUES ("Breno Elias Costa","Rua Catuaba","946","Jardim Colibrí","Campo Grande","MS");
+INSERT INTO sintomatico (nome,endereco,numero,bairro,cidade,uf) VALUES ("Sandra Débora Barros","Rua Maiêutica","360","Renascer","Macapá","AP");
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser');
 
 var db = require("./database.js");
 
 var HTTP_PORT = 8000;
 
 app.listen(HTTP_PORT, () => {
-  console.log(`Servidor executando na porta %PORT`.replace(`%PORT`,HTTP_PORT))
+  console.log(`Servidor executando em https://localhost:%PORT`.replace(`%PORT`,HTTP_PORT))
 });
 
 //Consulta todos os estabelecimentos de saúde da cidade informada
@@ -27,31 +28,41 @@ app.get("/api/ubscidade/:cidade", function (req, res, next) {
 });
 
 //Insere paciente suspeito na tabela para consulta pelos orgãos de saúde
-//  FALTA ARRUMAR - NÃO FUNCIONA, TA DANDO ERRO
+
+app.use(bodyParser.urlencoded({extended : false}));
 app.post("/api/sintomatico/", function(req, res){
   
   let dados = {
     "nome": req.body.nome,
     "endereco": req.body.endereco,
+    "numero": req.body.numero,
     "bairro": req.body.bairro,
-    "cidade": req.body.cidade    
+    "cidade": req.body.cidade,
+    "uf": req.body.uf  
   }
 
-  // var sql2 = `INSERT INTO sintomaticos(nome,endereco,bairro,cidade) VALUES (?,?,?,?)`;
-  // var params2 = [dados.nome, dados.endereco, dados.bairro, dados.cidade];
+  var sql2 = `INSERT INTO sintomatico(nome,endereco,numero,bairro,cidade,uf) VALUES (?,?,?,?,?,?)`;
+  var params2 = [dados.nome, dados.endereco, dados.numero, dados.bairro, dados.cidade, dados.uf];
 
+  console.log(req.body);
   console.log(dados);
-  
-  // db.run(sql2, params2, function(err){
-  //     if(err){
-  //       res.status(400).json({"error": err.message})
-  //       return;
-  //     }
-  // });
+
+  res.on("finish", () => 
+    {console.log(res);
+  });
+
+  db.run(sql2, params2, function(err){
+      if(err){
+        res.status(400).json({"error": err.message})
+        return;
+      }
+      return res.json({success: true, msg: 'User registered'});
+  });
 });
+
 let row;
 app.get("/api/sintomatico", function(req, res){
-  let sql3 = `SELECT * FROM sintomaticos;`;
+  let sql3 = `SELECT * FROM sintomatico;`;
   console.log(sql3);
 
   db.all(sql3, [], function(err,row){
