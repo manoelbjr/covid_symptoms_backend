1abdf315948e17150d611f692cc9d21a8f3ef4c3
Manoel Barros <manoel.barros.junior@gmail.com>
2020-11-27 21:28:43 -0300

    Adicionada nova rota GET

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express = require("express");
-var app = express();
-var bodyParser = require('body-parser');
+let express = require("express");
+let app = express();
+let bodyParser = require('body-parser');
+const cors = require('cors');
+let db = require("./database.js");
 
-var db = require("./database.js");
-
-var HTTP_PORT = 8000;
+let HTTP_PORT = 8000;
 
 app.listen(HTTP_PORT, () => {
   console.log(`Servidor executando em https://localhost:%PORT`.replace(`%PORT`,HTTP_PORT))
@@ -30,6 +30,8 @@ app.get("/api/ubscidade/:cidade", function (req, res, next) {
 //Insere paciente suspeito na tabela para consulta pelos orgãos de saúde
 
 app.use(bodyParser.urlencoded({extended : false}));
+app.use(cors());
+
 app.post("/api/sintomatico/", function(req, res){
   
   let dados = {
@@ -44,39 +46,44 @@ app.post("/api/sintomatico/", function(req, res){
   var sql2 = `INSERT INTO sintomatico(nome,endereco,numero,bairro,cidade,uf) VALUES (?,?,?,?,?,?)`;
   var params2 = [dados.nome, dados.endereco, dados.numero, dados.bairro, dados.cidade, dados.uf];
 
-  console.log(req.body);
-  console.log(dados);
-
-  res.on("finish", () => 
-    {console.log(res);
-  });
-
   db.run(sql2, params2, function(err){
       if(err){
         res.status(400).json({"error": err.message})
         return;
       }
-      return res.json({success: true, msg: 'User registered'});
+      //res.status(201);
+      res.json({success: true});
+      console.log("success:", res.json());
+      
   });
 });
 
 let row;
+
 app.get("/api/sintomatico", function(req, res){
   let sql3 = `SELECT * FROM sintomatico;`;
-  console.log(sql3);
+  //console.log(sql3);
 
   db.all(sql3, [], function(err,row){
-
-    row.forEach((row)=>{
-      console.log(row);
-    });
-
-    res.json({
-      "message": "success",
-      "data": row
-    });
-
+    
+    res.json(row);
+    console.log(res.json());
   });
+});
+
+app.get("/api/sintomatico/getlast", function(req,res){
+  let query = `SELECT nome,cidade, uf FROM ( select * from sintomatico ORDER BY ID desc limit 1) ORDER BY id;`;
+  let output;
+
+  db.all(query, [], function(err, output){
+    if(err){
+      output.status(400).json({"error":err.message});
+    }
+
+    res.json(output);
+    console.log(query);
+  });
+
 });
 
 app.use(function(req, res){
